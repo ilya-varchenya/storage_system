568b905393b89e149376bcfd8de4e60d9980f936
ilya-varchenya <ilya.varchenya@gmail.com>
2022-11-29 19:16:22 +0300

    add electronics

--- constants.py
+++ constants.py
@@ -10,3 +10,30 @@ SEARCH_ITEM = 'найти'
 # storage cells
 UPPER_LEFT = [0, 0]
 BOTTOM_RIGHT = [3, 3]
+
+LED1 = 25
+LED2 = 24
+LED3 = 26
+LED4 = 13
+LED5 = 7
+LED6 = 8
+LED7 = 6
+LED8 = 19
+LED9 = 12
+LED10 = 16
+LED11 = 5
+LED12 = 11
+LED13 = 20
+LED14 = 21
+LED15 = 10
+LED16 = 9
+
+LEDS = [LED1,  LED2,  LED3,  LED4,
+        LED5,  LED6,  LED7,  LED8,
+        LED9,  LED10, LED11, LED12,
+        LED13, LED14, LED15, LED16]
+
+# leds = [25, 24, 26, 13,
+#         7,  8,  6,  19,
+#         12, 16, 5,  11,
+#         20, 21, 10, 9]
--- electronics.py
+++ electronics.py
@@ -1,3 +1,8 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+from constants import LEDS
+
 def get_data_from_wage_module():
     pass
 
@@ -11,4 +16,50 @@ def indicate_cell(line, column):
 
 
 def indicate_activation():
-    pass
+    pause_time = 0.5
+    GPIO.output(LEDS[0], GPIO.HIGH)
+    GPIO.output(LEDS[1], GPIO.HIGH)
+    GPIO.output(LEDS[2], GPIO.HIGH)
+    GPIO.output(LEDS[3], GPIO.HIGH)
+    GPIO.output(LEDS[12], GPIO.HIGH)
+    GPIO.output(LEDS[13], GPIO.HIGH)
+    GPIO.output(LEDS[14], GPIO.HIGH)
+    GPIO.output(LEDS[15], GPIO.HIGH)
+    sleep(pause_time)
+    GPIO.output(LEDS[0], GPIO.LOW)
+    GPIO.output(LEDS[1], GPIO.LOW)
+    GPIO.output(LEDS[2], GPIO.LOW)
+    GPIO.output(LEDS[3], GPIO.LOW)
+    GPIO.output(LEDS[12], GPIO.LOW)
+    GPIO.output(LEDS[13], GPIO.LOW)
+    GPIO.output(LEDS[14], GPIO.LOW)
+    GPIO.output(LEDS[15], GPIO.LOW)
+    
+    GPIO.output(LEDS[4], GPIO.HIGH)
+    GPIO.output(LEDS[5], GPIO.HIGH)
+    GPIO.output(LEDS[6], GPIO.HIGH)
+    GPIO.output(LEDS[7], GPIO.HIGH)
+    GPIO.output(LEDS[8], GPIO.HIGH)
+    GPIO.output(LEDS[9], GPIO.HIGH)
+    GPIO.output(LEDS[10], GPIO.HIGH)
+    GPIO.output(LEDS[11], GPIO.HIGH)
+    sleep(pause_time)
+    GPIO.output(LEDS[4], GPIO.LOW)
+    GPIO.output(LEDS[5], GPIO.LOW)
+    GPIO.output(LEDS[6], GPIO.LOW)
+    GPIO.output(LEDS[7], GPIO.LOW)
+    GPIO.output(LEDS[8], GPIO.LOW)
+    GPIO.output(LEDS[9], GPIO.LOW)
+    GPIO.output(LEDS[10], GPIO.LOW)
+    GPIO.output(LEDS[11], GPIO.LOW)
+    
+if __name__ == '__main__':
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.cleanup()
+    
+    for i in LEDS:
+        GPIO.setup(i, GPIO.OUT, initial=GPIO.LOW)
+    
+    indicate_activation()
+
